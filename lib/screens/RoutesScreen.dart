24de16194d8910430eca63cae4bf6dd52380d79d
Kvan7 <71402892+Kvan7@users.noreply.github.com>
2022-09-17 18:42:48 -0500

    Added Route list elements

--- lib/screens/RoutesScreen.dart
+++ lib/screens/RoutesScreen.dart
@@ -1,11 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+// import 'package:provider/provider.dart';
 
-class RoutesScreen extends StatelessWidget {
+class RoutesScreen extends StatefulWidget {
   const RoutesScreen({super.key});
+  @override
+  State<RoutesScreen> createState() => _RoutesScreenState();
+}
+
+final List<String> routeList = <String>['A', 'B', 'C'];
+final List<double> riskList = <double>[0.5, 0.1, 0.4];
+final List<int> timeSheet = <int>[10, 23, 1];
+
+class _RoutesScreenState extends State<RoutesScreen> {
+  final _biggerFont = const TextStyle(fontSize: 24);
+
+  final int min = 0;
+  final int max = 10;
+  int randomNumber = 1;
 
   @override
   Widget build(BuildContext context) {
-    return (Text("Routes Screen"));
+    return Align(
+      alignment: Alignment.topCenter,
+      child: ListView.separated(
+        padding: const EdgeInsets.all(16),
+        itemCount: 3,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 50,
+            color: const Color.fromARGB(255, 82, 172, 94),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    'Route ${routeList[index]}',
+                    style: _biggerFont,
+                  ),
+                ),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Text('Risk: ${riskList[index]}'),
+                  ),
+                ),
+                Expanded(
+                  child: Text('time: ${timeSheet[index]}'),
+                ),
+              ],
+            ),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+      ),
+    );
   }
 }
