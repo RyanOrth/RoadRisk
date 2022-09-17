4b9d23ab62fe9c1330934587a6c075de8b7ecc92
Gabriel Clark <>
2022-09-17 16:54:40 -0500

    Created seperation for each page in app.

---
+++ lib/screens/RoutesScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class RoutesScreen extends StatelessWidget {
+  const RoutesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return (Text("Routes Screen"));
+  }
+}
