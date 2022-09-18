dae4e7e2e5ffa4ece89a05445dbd5c2d760f2bbf
Timothy Gibbons <tjgibbons10@gmail.com>
2022-09-18 03:41:04 -0500

    Formatting and testing polylines

--- lib/screens/MapScreen.dart
+++ lib/screens/MapScreen.dart
@@ -14,174 +14,156 @@ import 'package:road_risk/utils/ViewportPainter.dart';
 
 
 class MapScreen extends StatefulWidget {
-  const MapScreen({Key? key}) : super(key: key);
+	const MapScreen({Key? key}) : super(key: key);
 
-  @override
-  MapScreenState createState() => MapScreenState();
+	@override
+	MapScreenState createState() => MapScreenState();
 }
 
 class MapScreenState extends State<MapScreen> {
-  final controller = MapController(
-    location: const LatLng(0, 0),
-    zoom: 6,
-  );
+	final controller = MapController(
+		location: const LatLng(0, 0),
+		zoom: 6,
+	);
 
-  bool _darkMode = false;
+	bool _darkMode = false;
 
-  static const polyCoords = [
-    LatLng(40, -60),
-    LatLng(20, -20),
-    LatLng(0, -10),
-    LatLng(10, 0),
-    LatLng(0, 10),
-    LatLng(20, 20),
-    LatLng(0, 60),
-  ];
+	static const polyCoords = [
+		LatLng(40.8205124, -96.7040093),
+		LatLng(40.8136168, -96.7040556),
+		LatLng(40.8136231, -96.7055320),
+		LatLng(40.8118775, -96.7055320),
+	];
 
-  void _gotoDefault() {
-    controller.center = const LatLng(0,0);
-    controller.zoom = 14;
-    setState(() {});
-  }
+	void _gotoDefault() {
+		controller.center = const LatLng(0,0);
+		controller.zoom = 14;
+		setState(() {});
+	}
 
-  void _onDoubleTap(MapTransformer transformer, Offset position) {
-    const delta = 10;
-    final zoom = clamp(controller.zoom + delta, 2, 18);
+	void _onDoubleTap(MapTransformer transformer, Offset position) {
+		const delta = 10;
+		final zoom = clamp(controller.zoom + delta, 2, 18);
 
-    transformer.setZoomInPlace(zoom, position);
-    setState(() {});
-  }
+		transformer.setZoomInPlace(zoom, position);
+		setState(() {});
+	}
 
-  Offset? _dragStart;
-  double _scaleStart = 1.0;
-  void _onScaleStart(ScaleStartDetails details) {
-    _dragStart = details.focalPoint;
-    _scaleStart = 1.0;
-  }
+	Offset? _dragStart;
+	double _scaleStart = 1.0;
+	void _onScaleStart(ScaleStartDetails details) {
+		_dragStart = details.focalPoint;
+		_scaleStart = 1.0;
+	}
 
-  void _onScaleUpdate(ScaleUpdateDetails details, MapTransformer transformer) {
-    final scaleDiff = details.scale - _scaleStart;
-    _scaleStart = details.scale;
+	void _onScaleUpdate(ScaleUpdateDetails details, MapTransformer transformer) {
+		final scaleDiff = details.scale - _scaleStart;
+		_scaleStart = details.scale;
 
-    if (scaleDiff > 0) {
-      controller.zoom += 10;
+		if (scaleDiff > 0) {
+			controller.zoom += 10;
+			setState(() {});
+			return;
+		} 
 
-      setState(() {});
-    } else if (scaleDiff < 0) {
-      controller.zoom -= 10;
-      if (controller.zoom < 1) {
-        controller.zoom = 1;
-      }
-      setState(() {});
-    } else {
-      final now = details.focalPoint;
-      var diff = now - _dragStart!;
-      _dragStart = now;
-      final h = transformer.constraints.maxHeight;
+		if (scaleDiff < 0) {
+			controller.zoom -= 10;
+			if (controller.zoom < 1) {
+				controller.zoom = 1;
+			}
+			setState(() {});
+			return;
+		}
 
-      final vp = transformer.getViewport();
-      if (diff.dy < 0 && vp.bottom - diff.dy < h) {
-        diff = Offset(diff.dx, 0);
-      }
+		final now = details.focalPoint;
+		var diff = now - _dragStart!;
+		_dragStart = now;
+		final h = transformer.constraints.maxHeight;
 
-      if (diff.dy > 0 && vp.top - diff.dy > 0) {
-        diff = Offset(diff.dx, 0);
-      }
+		final vp = transformer.getViewport();
+		if (diff.dy < 0 && vp.bottom - diff.dy < h) {
+			diff = Offset(diff.dx, 0);
+		}
 
-      transformer.drag(diff.dx, diff.dy);
-      setState(() {});
-    }
-  }
+		if (diff.dy > 0 && vp.top - diff.dy > 0) {
+			diff = Offset(diff.dx, 0);
+		}
 
-  @override
-  Widget build(BuildContext context) {
-    final polylines = <Polyline>[
-      Polyline(
-        data: polyCoords,
-        paint: Paint()
-          ..strokeWidth = 4
-          ..color = Colors.red,
-      )
-    ];
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Raster Map'),
-        actions: [
-          IconButton(
-            tooltip: 'Toggle Dark Mode',
-            onPressed: () {
-              setState(() {
-                _darkMode = !_darkMode;
-              });
-            },
-            icon: const Icon(Icons.wb_sunny),
-          ),
-        ],
-      ),
-      body: MapLayout(
-        controller: controller,
-        builder: (context, transformer) {
-          return GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onDoubleTapDown: (details) => _onDoubleTap(
-              transformer,
-              details.localPosition,
-            ),
-            onScaleStart: _onScaleStart,
-            onScaleUpdate: (details) => _onScaleUpdate(details, transformer),
-            child: Listener(
-              behavior: HitTestBehavior.opaque,
-              onPointerSignal: (event) {
-                if (event is PointerScrollEvent) {
-                  final delta = event.scrollDelta.dy / -100.0;
-                  final zoom = clamp(controller.zoom + delta, 2, 18);
+		transformer.drag(diff.dx, diff.dy);
+		setState(() {});
+	}
 
-                  transformer.setZoomInPlace(zoom, event.localPosition);
-                  setState(() {});
-                }
-              },
-              child: Stack(
-                children: [
-                  TileLayer(
-                    builder: (context, x, y, z) {
-                      final tilesInZoom = pow(2.0, z).floor();
+	@override
+	Widget build(BuildContext context) {
+		final polylines = <Polyline>[
+			Polyline(
+				data: polyCoords,
+				paint: Paint()
+				  ..strokeWidth = 4
+				  ..color = Colors.red,
+			)
+		];
+		
+		return MapLayout(
+			controller: controller,
+			builder: (context, transformer) {
+				return GestureDetector(
+					behavior: HitTestBehavior.opaque,
+					onDoubleTapDown: (details) => _onDoubleTap(
+						transformer,
+						details.localPosition,
+					),
+					onScaleStart: _onScaleStart,
+					onScaleUpdate: (details) => _onScaleUpdate(details, transformer),
+					child: Listener(
+						behavior: HitTestBehavior.opaque,
+						onPointerSignal: (event) {
+							if (!(event is PointerScrollEvent)) {
+								return;
+							}
+							final delta = event.scrollDelta.dy / -100.0;
+							final zoom = clamp(controller.zoom + delta, 2, 18);
 
-                      while (x < 0) {
-                        x += tilesInZoom;
-                      }
-                      while (y < 0) {
-                        y += tilesInZoom;
-                      }
+							transformer.setZoomInPlace(zoom, event.localPosition);
+							setState(() {});
+						},
+						child: Stack(
+							children: [
+								TileLayer(
+									builder: (context, x, y, z) {
+										final tilesInZoom = pow(2.0, z).floor();
 
-                      x %= tilesInZoom;
-                      y %= tilesInZoom;
+										while (x < 0) {
+											x += tilesInZoom;
+										}
+										while (y < 0) {
+											y += tilesInZoom;
+										}
 
-                      return CachedNetworkImage(
-                        imageUrl: OSM(z, x, y),
-                        fit: BoxFit.cover,
-                      );
-                    },
-                  ),
-                  PolylineLayer(
-                    transformer: transformer,
-                    polylines: polylines,
-                  ),
-                  CustomPaint(
-                    painter: ViewportPainter(
-                      transformer.getViewport(),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _gotoDefault,
-        tooltip: 'My Location',
-        child: const Icon(Icons.my_location),
-      ),
-    );
-  }
+										x %= tilesInZoom;
+										y %= tilesInZoom;
+
+										return CachedNetworkImage(
+											imageUrl: OSM(z, x, y),
+											fit: BoxFit.cover,
+										);
+									},
+								),
+								PolylineLayer(
+									transformer: transformer,
+									polylines: polylines,
+								),
+								CustomPaint(
+									painter: ViewportPainter(
+										transformer.getViewport(),
+									),
+								),
+							],
+						),
+					),
+				);
+			},
+		);
+	}
+
 }
