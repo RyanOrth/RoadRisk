bdc59503afdc5abf45e07ebab7064774fe48e2d1
Gabriel Clark <>
2022-09-18 00:13:04 -0500

    Added rudimentary waypoints on map for origin and destination.

--- lib/screens/MapsScreen.dart
+++ lib/screens/MapsScreen.dart
@@ -10,8 +10,11 @@ class MapsScreen extends StatefulWidget {
 
 class _MapsScreenState extends State<MapsScreen> {
   late GoogleMapController mapController;
+  Marker _origin = const Marker(markerId: MarkerId("origin"), visible: false);
+  Marker _destination =
+      const Marker(markerId: MarkerId("destination"), visible: false);
 
-  final LatLng _center = const LatLng(45.521563, -122.677433); // Portland
+  final LatLng _center = const LatLng(37.773972, -122.432917); // San Francisco
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -31,8 +34,51 @@ class _MapsScreenState extends State<MapsScreen> {
             target: _center,
             zoom: 11.0,
           ),
+          markers: {
+            if (_origin.visible) _origin,
+            if (_destination.visible) _destination,
+          },
+          onTap: _addMarker,
           zoomControlsEnabled: false,
           myLocationButtonEnabled: false,
         )));
+  }
+
+  void _addMarker(LatLng pos) {
+    if (!_origin.visible || (!_origin.visible && !_destination.visible)) {
+      // Origin is not set OR Origin/Destination are both set
+      // Set origin
+      setState(() {
+        _origin = Marker(
+          markerId: const MarkerId('origin'),
+          infoWindow: const InfoWindow(title: 'Origin'),
+          icon:
+              BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+          position: pos,
+          visible: true,
+        );
+        // Reset destination
+        _destination =
+            const Marker(markerId: MarkerId("destination"), visible: false);
+      });
+    } else {
+      // Origin is already set
+      // Set destination
+      setState(() {
+        _destination = Marker(
+          markerId: const MarkerId('destination'),
+          infoWindow: const InfoWindow(title: 'Destination'),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+          position: pos,
+          visible: true,
+        );
+      });
+      /* 
+      // Get directions
+      final directions = await DirectionsRepository()
+          .getDirections(origin: _origin.position, destination: pos);
+      setState(() => _info = directions);
+      */
+    }
   }
 }
