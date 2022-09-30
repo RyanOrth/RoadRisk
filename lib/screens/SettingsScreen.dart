d1a51293620e42b2e80eb6db8c2c4a386271e835
Gabriel Clark <gabrielclark3330@gmail.com>
2022-09-30 14:55:31 -0500

    Added start of text to tutorial screen.

--- lib/screens/SettingsScreen.dart
+++ lib/screens/SettingsScreen.dart
@@ -12,9 +12,30 @@ class _SettingsScreenState extends State<SettingsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.topCenter,
-      child: Text("Tutorial"),
+    return Container(
+      margin: const EdgeInsets.only(top: 50, left: 50, right: 50),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        //crossAxisAlignment: CrossAxisAlignment.start,
+        children: const <Widget>[
+          Text(
+            "Maps Screen",
+            style: TextStyle(
+              fontSize: 40,
+              color: Colors.blueGrey,
+            ),
+          ),
+          Divider(),
+          SizedBox(height: 40),
+          Text(
+            "When starting the app, navigate to the maps page and select two points on the map by tapping the desired locations.",
+            style: TextStyle(
+              fontSize: 20,
+              color: Color.fromARGB(255, 35, 42, 46),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
